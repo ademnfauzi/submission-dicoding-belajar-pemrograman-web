f048f96cdba94b6f2ecc125a3562306be41be608
Ade Muhammad Nur Fauzi <ademnfauzi76@gmail.com>
2022-06-23 15:32:32 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function bigImg(x) {
+    x.style.height = "164px";
+    x.style.width = "164px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "100px";
+    x.style.width = "100px";
+  }
